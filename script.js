11e99a30a23763630df1038f6c91123e1def3472
atvars <avelavs@gmail.com>
2024-04-14 21:32:53 +0000

    getting and displayoing joke last commit

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-// getting my joke id div and button
-const jokeEl = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
+// getting my joke div and button
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+// adding a event listener to button to call a function when button is pressed
+jokeBtn.addEventListener('click', generateJoke);
 
 // function to fetch a joke from API and setting response to be in JSON format
+// calling a functionon page load
+generateJoke();
 
-generateJoke()
+async function generateJoke() {
+  //storing response to be in JSON format in a variable by setting headers
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
 
-function generateJoke() {
-    //storing response to be in JSON format in a variable by setting headers
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        }
-    }
+  // fetch
+  const response = await fetch('https://icanhazdadjoke.com', config);
 
-    fetch('https://icanhazdadjoke.com', config)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
+  // to JSON
+  const data = await response.json();
+
+  // display joke
+  jokeEl.innerHTML = data.joke;
 }
